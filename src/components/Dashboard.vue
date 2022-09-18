0f0067d6d00f03738fcfb5988182cd1bb0a1e563
David Campos <davidcamposcruz2016@gmail.com>
2022-09-17 22:38:49 -0300

    Pedidos CSS

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -4,25 +4,27 @@
             <div id="pizza-table-heading">
                 <div class="order-id">#:</div>
                 <div>Cliente:</div>
-                <div>Pizza:</div>
-                <div>Opcional:</div>
+                <div>Endereço:</div>
+                <div>Sabor:</div>
+                <div>Opcionais:</div>
                 <div>Ações:</div>
             </div>
         </div>
         <div id="pizza-table-rows">
             <div class="pizza-table-row">
                 <div class="order-number">1</div>
-                <div>Nome do Cliente</div>
-                <div>Pizza</div>
+                <div>David</div>
+                <div>Rua ABC 123</div>
+                <div>Marguerita</div>
                 <div>
                     <ul>
-                        <li>Azeitona</li>
-                        <li>Borda Crocante</li>
+                        <li>Sem cebola</li>
+                        <li>Com bacon</li>
                     </ul>
                 </div>
                 <div>
                     <select name="status" class="status">
-                        <option value="">Selecione o status do pedido</option>
+                        <option value="">Selecione</option>
                     </select>
                     <button class="delete-btn">Cancelar</button>
                 </div>
@@ -44,7 +46,7 @@
     }
 
     #pizza-table-heading ,
-    #pizza-table-rows , 
+    #pizza-table-rows ,
     .pizza-table-row {
         display: flex;
         flex-wrap: wrap;
@@ -58,17 +60,17 @@
 
     #pizza-table-heading div ,
     .pizza-table-row div {
-        width: 20%;
+        width: 19%;
     }
 
     .pizza-table-row {
         width: 100%;
         padding: 12px;
-        border-bottom: 1px solid #CCC
+        border-bottom: 1px solid #CCC;
     }
 
-    #pizza-table-heading .order-id , 
-    .pizza-table-row .order.number {
+    #pizza-table-heading .order-id ,
+    .pizza-table-row .order-number {
         width: 5%;
     }
 
@@ -79,9 +81,10 @@
 
     .delete-btn {
         background-color: #222;
-        color:#FCBA03;
+        color: #FCBA03;
         font-weight: bold;
         border: 2px solid #222;
+        border-radius: 2px;
         padding: 10px;
         font-size: 16px;
         margin: 0 auto;
